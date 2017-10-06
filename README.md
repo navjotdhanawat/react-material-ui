693ae5706e12f611ca071f06bca79a17ea5e7eca
Navjot Dhanawat <n.dhanawat@splendornet.com>
2017-10-06 18:52:04 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## React Material UI
+
+[How to use material ui with react js](http://www.thenextfact.com/use-material-ui-react-js-tutorial/)
---
+++ src/Mycomponent.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
+const style = {
+  margin: 12,
+};
+const MyComponent = () => (
+  <div>
+    <RaisedButton label="Default" style={style} />
+    <RaisedButton label="Primary" primary={true} style={style} />
+    <RaisedButton label="Secondary" secondary={true} style={style} />
+    <RaisedButton label="Disabled" disabled={true} style={style} />
+    <br />
+    <br />
+    <RaisedButton label="Full width" fullWidth={true} />
+  </div>
+);
+
+export default MyComponent;
